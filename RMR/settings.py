448d968a1614637a63545cd009679a1aab51c15c
t17cs040 <t17cs040@yamanashi.ac.jp>
2019-12-13 13:52:14 +0900

    [test]
    test

--- RMR/settings.py
+++ RMR/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'ja' 
 
 TIME_ZONE = 'Asia/Tokyo'
 
