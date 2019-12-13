ffd51f95ee168d6a15df475c0386c0775c5902d8
soma-lac <love.as.cat@gmail.com>
2019-12-13 18:55:23 +0900

    [add]database設計
    データベースのフレームのようなもの完成．作っていて気になったことをメモ
    ・会議室データに料金情報を付与すると時間，半日，一日貸しの分割における処理が面倒になりそう．
    具体的に言えば，料金が利用方法によって一意に定まらないのでデータベースで固定値として入力できない

--- Reservation/admin.py
+++ Reservation/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Member,Reserve,MeetingRoom,Facility,Billing
 
 # Register your models here.
+admin.site.register(Member)
+admin.site.register(Reserve)
+admin.site.register(MeetingRoom)
+admin.site.register(Facility)
+admin.site.register(Billing)
--- Reservation/models.py
+++ Reservation/models.py
@@ -11,6 +11,8 @@ class Member(models.Model):
     menber_name = models.CharField(max_length = 50)
     mail = models.CharField(max_length = 50)
     payment = models.IntegerField()
+    def __str__(self):
+        return self.company_name
     
 class Reserve(models.Model):
     rsv_number = models.IntegerField()
@@ -20,19 +22,27 @@ class Reserve(models.Model):
     name_rsvMR = models.CharField(max_length = 50)
     name_rsvFcl = models.CharField(max_length = 50)
     charge_rsv = models.IntegerField()
+    def __str__(self):
+        return self.rsv_number
     
 class MeetingRoom(models.Model):
     name_MR = models.CharField(max_length = 50)
     availability = models.IntegerField()
     charge_MR = models.IntegerField()
+    def __str__(self):
+        return self.name_MR
     
 class Facility(models.Model):
     name_facility = models.CharField(max_length = 50)
     stock = models.CharField(max_length = 50)
     charge_fcl = models.IntegerField()
+    def __str__(self):
+        return self.name_facility
     
 class Billing(models.Model):
-    bill_id = models.ImageField()
+    bill_id = models.IntegerField()
     amount = models.IntegerField()
     remain = models.IntegerField()
+    def __str__(self):
+        return self.bill_id
     
