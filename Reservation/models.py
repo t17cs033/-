07d6889e3b34bcf08a475ca4bb34c097a4d92b40
t17cs040 <t17cs040@yamanashi.ac.jp>
2019-12-13 15:02:07 +0900

    [send] to my pc

--- Reservation/models.py
+++ Reservation/models.py
@@ -4,7 +4,7 @@ from unittest.util import _MAX_LENGTH
 # Create your models here.
 class Member（models.Model):
     member_id = models.IntegerField()
-    company_name = models.CharField(max_length = 200)
+    company_name = models.CharField(max_length = 100)
     company_address = models.CharField(max_length = 200)
     
 
