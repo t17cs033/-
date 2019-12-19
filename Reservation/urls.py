87ebf4953b096dfa57804e8e1842420efebde3df
soma-lac <love.as.cat@gmail.com>
2019-12-19 10:22:53 +0900

    Revert "[toMyPC]自身の開発環境へ送るための中断"
    This reverts commit 4a6a8797facedfeec09ec73a759343dd0a240d75.

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
 from . import views
-from Reservation.views import BillingView
 
-appname='Reservation'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('bill',BillingView.as_view(),name='bill'),
 ]
