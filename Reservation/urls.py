8fe0ccc387d70de8d8f9cf8afff63f2a2d2ca26b
t17cs048 <t17cs048@jw07.kki.yamanashi.ac.jp>
2019-12-27 00:44:24 +0900

    予約一覧と予約詳細の作成。URL指定で飛べる状態

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 from . import views
+from django.urls.conf import path
+from Reservation.views import ReserveList, ReserveDetal
+
+appname = "Reservation"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('reserve_list/', ReserveList.as_view(), name = 'reserve_list'),
+    path('reserve_list/<int:pk>/', ReserveDetal.as_view(), name = 'reserve_detail'),
 ]
--- Reservation/views.py
+++ Reservation/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from Reservation.models import Reserve
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. ")
+
+class ReserveList(ListView):
+    model = Reserve
+    
+class ReserveDetal(DetailView):
+    
+    model = Reserve
