79172279c85f9575000e22f105fb1540a8e4ae9b
t17cs014 <t17cs014@yamanashi.ac.jp>
2019-12-29 17:21:49 +0900

    [add]各画面表示

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
 from . import views
+from .views import MRShowView, BigMRReservationView, MiddleMRReservationView, SmallMRReservationView
+from .views import ACornerReservationView, BCornerReservationView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('mrshow', MRShowView.as_view(),name='mrshow'),
+    path('mrbig', BigMRReservationView.as_view(),name='mrbig'),
+    path('mrmid', MiddleMRReservationView.as_view(),name='mrmid'),
+    path('mrsml', SmallMRReservationView.as_view(),name='mrsml'),
+    path('cora', ACornerReservationView.as_view(),name='cora'),
+    path('corb', BCornerReservationView.as_view(),name='corb'),
 ]
--- Reservation/views.py
+++ Reservation/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import MeetingRoom
+from django.views.generic.base import TemplateView
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. ")
+
+class MRShowView(TemplateView):
+    model = MeetingRoom
+    template_name = 'Reservation/mr_show.html'
+    
+class BigMRReservationView(TemplateView):
+    model = MeetingRoom
+    template_name = 'Reservation/mr_big_reservation.html'
+    
+class MiddleMRReservationView(TemplateView):
+    model = MeetingRoom
+    template_name = 'Reservation/mr_middle_reservation.html'
+    
+class SmallMRReservationView(TemplateView):
+    model = MeetingRoom
+    template_name = 'Reservation/mr_small_reservation.html'
+    
+class ACornerReservationView(TemplateView):
+    model = MeetingRoom
+    template_name = 'Reservation/corner_a_reservation.html'
+    
+class BCornerReservationView(TemplateView):
+    model = MeetingRoom
+    template_name = 'Reservation/corner_b_reservation.html'
