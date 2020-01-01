12953717c8fe1bca5f1ccdaf9b4429419bc2d273
soma-lac <love.as.cat@gmail.com>
2020-01-02 02:48:40 +0900

    [add]料金案内画面追加
    画面は出る．データの内容表示で苦戦中

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from . import views
-from .views import BillingView, BillingBase
+from .views import BillingView, BillingBase, GuideView
 
 appname='Reservation'
 urlpatterns = [
     path('', views.index, name='index'),
     path('Billing/',BillingBase.as_view(),name='billbase'),
     path('Billing/<int:pk>/',BillingView.as_view(),name='billing'),
+    path('PriceGuide',GuideView.as_view(),name='guide')
 ]
--- Reservation/views.py
+++ Reservation/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Billing
 from django.views.generic.list import ListView
-from django.views.generic.base import TemplateView
+from Reservation.models import MeetingRoom, Facility
 
 # Create your views here.
 
@@ -19,3 +19,17 @@ class BillingBase(ListView):
 class BillingView(DetailView):
     model = Billing
     template_name = "Reservation/Billing.html"
+    
+class GuideView(ListView):
+    model = MeetingRoom
+    template_name = "Reservation/Guide.html"
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.update(
+            {
+                'fcl_list' : Facility.objects.order_by('fclName'),
+                'extra' : Facility.objects.all(),
+            }
+        )
+        return ctx
