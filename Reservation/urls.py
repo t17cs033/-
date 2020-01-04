c63d5910b45d4bbf2fdaf5d9666e3c797fa4a1b6
龍二 <56064176+t17cs033@users.noreply.github.com>
2020-01-04 16:19:17 +0900

    やったこと
    ・login画面の作成
    ・select画面の作成
    ・請求画面（テスト用）を作成
    ・cssファイルの作成

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import LoginView,SelectView,Select
+from Reservation.views import BillingTestView, BillingTest
 
-app_name = 'Reservation'
+appname = 'Reservation'
 urlpatterns = [
     path('login',LoginView.as_view(),name='login'),
     path('select',SelectView.as_view(),name ='select'),
-    path('<int:pk>/select',Select.as_view(),name='sel'),
+    path('select/<int:pk>',Select.as_view(),name='sel'),
+    path('billing',BillingTestView.as_view(),name ='billing'),
+    path('billing/<int:pk>',BillingTest.as_view(),name='t_b'),
 ]
--- Reservation/views.py
+++ Reservation/views.py
@@ -10,15 +10,16 @@ from django.views.generic.edit import UpdateView
 
 # Create your views here.
 
-class LoginView(ListView):
+class LoginView(TemplateView):
     template_name = 'Reservation/login.html'
     model = Member
 
     def post(self, request, *args, **kwargs):
         cmpId = self.request.POST.get('cmpId')
+        member = Member.objects.get(pk = cmpId)
         context = super().get_context_data(**kwargs)
-        member = get_object_or_404(Member, pk=cmpId)
         context['form_id'] = MemberIdForm()
+        context['member'] = member
         return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
@@ -27,25 +28,52 @@ class LoginView(ListView):
         return context
 
 
-class SelectView(ListView):
+class SelectView(TemplateView):
     model = Member
     template_name = 'Reservation/select.html'
 
     def post(self,request,*args,**kwargs):
         cmpId =self.request.POST.get('cmpId')
-        member = get_object_or_404(Member, pk=cmpId)
-        return HttpResponseRedirect(reverse('select'))
+        context = super().get_context_data(**kwargs)
+        context['member'] = MemberForm()
+        return self.render_to_response(context)
 
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
-        context['form_id'] = MemberIdForm()
+        context['form_id'] = MemberIdForm(initial ={'cmpId':self.kwargs.get('pk')})
         context['form'] = MemberForm()
         return context
 
 class Select(UpdateView):
     model = Member
     template_name = 'Reservation/select.html'
-    fields = ('cmpId','cmpName','address','tel','section','name','mail',' pay')
+    fields = ('cmpId','cmpName','address','tel','section','name','mail','pay')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_id']=MemberIdForm(initial ={'cmpId':self.kwargs.get('pk')})
+        return context
+
+class BillingTestView(TemplateView):
+    model = Member
+    template_name = 'Reservation/test_billing.html'
+
+    def post(self,request,*args,**kwargs):
+        cmpId =self.request.POST.get('cmpId')
+        context = super().get_context_data(**kwargs)
+        context['member'] = MemberForm()
+        return self.render_to_response(context)
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_id'] = MemberIdForm(initial ={'cmpId':self.kwargs.get('pk')})
+        context['form'] = MemberForm()
+        return context
+
+class BillingTest(UpdateView):
+    model = Member
+    template_name = 'Reservation/test_billing.html'
+    fields = ('cmpId','cmpName','address','tel','section','name','mail','pay')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
