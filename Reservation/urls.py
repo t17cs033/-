04357dfa97fb1eb9b76427c2218a0769cfb4fd17
soma-lac <love.as.cat@gmail.com>
2020-01-06 17:26:43 +0900

    [fix]マージって難しい

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('Billing/',BillingBase.as_view(),name='billbase'),
     path('Billing/<int:pk>/',BillingView.as_view(),name='billing'),
-    path('PriceGuide',GuideView.as_view(),name='guide')
+    path('PriceGuide',GuideView.as_view(),name='guide'),
 ]
