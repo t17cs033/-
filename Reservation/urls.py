0de368c6fd5ce595e330f43acbb6a71733b255a2
t17cs048 <t17cs048@aw05.kki.yamanashi.ac.jp>
2020-01-08 16:02:09 +0900

    予約一覧にキャンセル機能を追加、予約一覧に会社IDでフィルタをかけられるように変更（仮）

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from . import views
 from django.urls.conf import path
-from Reservation.views import ReserveList, ReserveDetal
-
+from Reservation.views import ReserveList, ReserveDetail, ReserveDelete
 appname = "Reservation"
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('reserve_list/', ReserveList.as_view(), name = 'reserve_list'),
-    path('reserve_list/<int:pk>/', ReserveDetal.as_view(), name = 'reserve_detail'),
+    path('reserve_list/<int:pk>/', ReserveDetail.as_view(), name = 'reserve_detail'),
+    path('reserve_list/<int:pk>/delete/', ReserveDelete.as_view(), name = 'delete'),
 ]
--- Reservation/views.py
+++ Reservation/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from Reservation.models import Reserve
+from django.template.context_processors import request
+from Reservation import models
+from django.http.response import HttpResponseRedirect
+from audioop import reverse
+from django.views.generic.edit import DeleteView, CreateView
+from django.contrib.messages.api import success
+from django.urls.base import reverse_lazy
+from django.db.models.query import QuerySet
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. ")
 
+
+class ReserveDelete(DeleteView):
+    model = Reserve
+    success_url = reverse_lazy('reserve_list')
+    
 class ReserveList(ListView):
+    queryset = Reserve.objects.filter(cmpId = 1)
+    
+class ReserveDetail(DetailView):
     model = Reserve
     
-class ReserveDetal(DetailView):
+
+
     
-    model = Reserve
+    
