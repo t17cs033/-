bf798faf070200a03223655e643f0a007568a0a8
t17cs016 <t17cs016@bw04.kki.yamanashi.ac.jp>
2020-01-10 12:47:18 +0900

    [add]空き状況以外の実装　
    空き状況以外は実装（仮）

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 from django.urls.conf import path
-from Reservation.views import ReserveList, ReserveDetal
+from Reservation.views import ReserveList, ReserveDetal,ReserveCalendar
 
 appname = "Reservation"
 
@@ -10,4 +10,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('reserve_list/', ReserveList.as_view(), name = 'reserve_list'),
     path('reserve_list/<int:pk>/', ReserveDetal.as_view(), name = 'reserve_detail'),
+    path('month/<int:year>/<int:month>/', views.ReserveCalendar.as_view(), name='month'),
+    path('reserve_calender/', ReserveCalendar.as_view(), name = 'reserve_calender'),
+    path('date/<int:pk>/<int:year>-<int:month>-<int:date>/', views.ReserveList.as_view(), name='date'),
+    
 ]
