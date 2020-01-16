62e21c78261a5ea06852ed80c6b298f727baa71e
soma-lac <love.as.cat@gmail.com>
2020-01-16 14:17:54 +0900

    [add]競合対応したはず．ちょくちょくエラーが見えるのは気にしない

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from .views import BillingView, BillingBase, GuideView
 from django.urls.conf import path
-from Reservation.views import LoginView,SelectView,Select,BillingTestView, BillingTest, ReservationTestView,ReservationTest,ReserveList, ReserveDetal
+from Reservation.views import LoginView,SelectView,Select,BillingTestView, BillingTest, ReservationTestView,ReservationTest,ReserveList, ReserveDetail,ReserveDelete
 from . import views
 urlpatterns = [
 
@@ -16,7 +16,6 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('reserve_list/', ReserveList.as_view(), name = 'reserve_list'),
-    path('reserve_list/<int:pk>/', ReserveDetal.as_view(), name = 'reserve_detail'),
     path('reserve_list/<int:pk>/', ReserveDetail.as_view(), name = 'reserve_detail'),
     path('reserve_list/<int:pk>/delete/', ReserveDelete.as_view(), name = 'delete'),
     path('fcl_add/',views.fcl, name = 'fcl'),
